43bf5f770310ea205e260ffa994d8d3260da3fc9
Radon Rosborough <radon.neon@gmail.com>
2018-05-04 21:35:08 -0700

    Add README.md and init.el

---
+++ README.md
@@ -0,0 +1,227 @@
+# Miscellaneous cheatsheet
+
+This document has a number of tips and tricks relevant to Emacs usage.
+
+## Command line
+
+Access the command line by opening Terminal.app, found in
+`/Applications/Utilities/Terminal.app`. You can type in a command and
+press enter (`RET`) to run it. Most people write terminal commands in
+this document by putting them after a `$`, but you don't type the `$`
+when you input the command.
+
+Ask which directory you are in:
+
+    $ pwd
+    /Users/raxod502/files/school
+
+Show the files in your current directory (note that this is a
+lowercase `l`, not a `1`):
+
+    $ ls -lA
+    total 0
+    drwxr-xr-x  7 raxod502  staff  238 Mar 10 15:40 bhs
+    drwxr-xr-x  6 raxod502  staff  204 Nov 14 12:58 hmc
+    drwxr-xr-x  6 raxod502  staff  204 Mar 10 16:27 old
+    drwxr-xr-x  2 raxod502  staff   68 Mar 10 16:27 temp
+
+Change directory (you can give a name listed by `ls -lA`, the symbol
+`..` to move up a directory, or a full path name):
+
+    $ pwd
+    /Users/raxod502/files/school
+    $ cd hmc
+    $ pwd
+    /Users/raxod502/files/school/hmc
+    $ cd ..
+    $ pwd
+    /Users/raxod502/files/school
+    $ cd /Users/raxod502
+    $ pwd
+    /Users/raxod502
+
+Install Emacs:
+
+    $ brew install emacs --devel --with-cocoa
+
+To make it so that you can launch Emacs from the Applications folder
+rather than just the command line, you should first check where it was
+installed to by running `brew info emacs` and checking for the line
+ending in an asterisk:
+
+    $ brew info emacs
+    emacs: stable 25.3 (bottled), devel 26.1-rc1, HEAD
+    GNU Emacs text editor
+    https://www.gnu.org/software/emacs/
+    /usr/local/Cellar/emacs/26.0.91 (3,992 files, 128.8MB)
+      Built from source on 2018-04-09 at 14:01:16 with: --with-cocoa --with-modules
+    /usr/local/Cellar/emacs/26.1-rc1 (3,992 files, 129.2MB) *
+      Built from source on 2018-04-16 at 19:39:23 with: --with-cocoa --with-modules
+    From: https://github.com/Homebrew/homebrew-core/blob/master/Formula/emacs.rb
+
+Then you can copy the application from there, and it will be runnable
+from the Applications folder:
+
+    $ cp -R /usr/local/Cellar/emacs/26.1-rc1/Emacs.app /Applications
+
+Start Emacs:
+
+    $ emacs
+
+Start Emacs inside the terminal (some features may not work, but some
+people prefer it this way):
+
+    $ emacs -nw
+
+## Basic Emacs navigation
+
+The keybinding notation is abbreviated for convenience. `C-b` means
+control-b, `M-f` means option-f (but Emacs calls option as "meta",
+hence the `M`). `S-<left>` means shift-leftarrow. There are also
+multi-key sequences. So `C-c M-J` would mean control-c, followed by
+meta-shift-j. The enter key is denoted by `RET`.
+
+Quit:
+
+    C-x C-c
+
+Edit a file in the current window (type in the path name, and press
+`RET`; note that you can press `TAB` to get help from Emacs with
+this):
+
+    C-x C-f
+
+Split the current window into two, top and bottom:
+
+    C-x 2
+
+Split the current window into two, left and right:
+
+    C-x 3
+
+Switch between windows (but you can also just click using the mouse):
+
+    C-x o
+
+Close the current window:
+
+    C-x 0
+
+Switch the current window to another file that you already opened
+(usually faster than using `C-x C-f`; `TAB` also works here, and if
+you just press `RET` without entering anything, Emacs usually picks a
+nice default):
+
+    C-x b
+
+## General Org usage
+
+Go to your todo list (as if you had used `C-x C-f` to find it):
+
+    C-c t
+
+Cycle visibility of current subtree:
+
+    TAB
+
+Cycle visibility of entire tree:
+
+    S-TAB
+
+Mark a TODO item as DONE:
+
+    C-c t
+
+Insert a new header:
+
+    M-RET
+
+Increase indentation level:
+
+    M-<right>
+
+Decrease indentation level:
+
+    M-<left>
+
+Go to previous item:
+
+    C-<up>
+
+Go to next item:
+
+    C-<down>
+
+Set deadline:
+
+    C-c C-d
+
+Set scheduled date:
+
+    C-c C-s
+
+## Org capture usage
+
+Capture a TODO item:
+
+    C-c c
+
+Insert completed item into todo list:
+
+    C-c C-c
+
+Cancel capture:
+
+    C-c C-k
+
+## Org clock usage
+
+Clock in on the current task:
+
+    C-c C-x C-i
+
+Clock out:
+
+    C-c C-x C-o
+
+Clock in to the last task you clocked out of:
+
+    C-c C-x C-x
+
+Clock into a task, selecting from a list of recent ones:
+
+    C-u C-c C-x C-i
+
+Jump to the currently or previously clocked task in your todo list:
+
+    C-c C-x C-j
+
+## Org agenda usage
+
+Open the agenda view:
+
+    C-c a
+
+Mark a TODO item as DONE:
+
+    t
+
+Change the deadline or scheduled date to one day earlier:
+
+    S-<left>
+
+Change the deadline or scheduled date to one day later:
+
+    S-<right>
+
+Clock in:
+
+    I
+
+Clock out:
+
+    O
+
+Save the file:
+
+    s
---
+++ init.el
@@ -0,0 +1,170 @@
+;; Emacs on macOS has a bug which prevents it from making secure
+;; network connections, which are required in order to install
+;; packages. We can work around that by following the instructions at
+;; https://emacs.stackexchange.com/a/18070/12534, which give us the
+;; following code:
+
+(unless (file-exists-p "/usr/local/etc/libressl/cert.pem")
+  (error "You should run 'brew install libressl' first"))
+
+(with-eval-after-load 'gnutls
+  (add-to-list 'gnutls-trustfiles "/usr/local/etc/libressl/cert.pem"))
+
+;; I may be biased here but I prefer to use the package manager I
+;; wrote (called straight.el) rather than the one that comes with
+;; Emacs (called package.el). This code sets it up; it's from
+;; https://github.com/raxod502/straight.el#getting-started.
+
+(let ((bootstrap-file (concat user-emacs-directory "straight/repos/straight.el/bootstrap.el"))
+      (bootstrap-version 3))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+;; Because I have not had enough time to fix all the bugs, there are
+;; still some problems with straight.el. One of them causes Org to
+;; print a warning, unless we use the following code to work around
+;; the bug. This code is from
+;; https://github.com/raxod502/straight.el#installing-org-with-straightel.
+
+(require 'subr-x)
+(straight-use-package 'git)
+
+(defun org-git-version ()
+  "The Git version of org-mode.
+Inserted by installing org-mode or when a release is made."
+  (require 'git)
+  (let ((git-repo (expand-file-name
+                   "straight/repos/org/" user-emacs-directory)))
+    (string-trim
+     (git-run "describe"
+              "--match=release\*"
+              "--abbrev=6"
+              "HEAD"))))
+
+(defun org-release ()
+  "The release version of org-mode.
+Inserted by installing org-mode or when a release is made."
+  (require 'git)
+  (let ((git-repo (expand-file-name
+                   "straight/repos/org/" user-emacs-directory)))
+    (string-trim
+     (string-remove-prefix
+      "release_"
+      (git-run "describe"
+               "--match=release\*"
+               "--abbrev=0"
+               "HEAD")))))
+
+(provide 'org-version)
+
+;; The main feature of this Emacs configuration is Org, so naturally
+;; we want to install the `org' package, which we can do using
+;; straight.el.
+
+(straight-use-package 'org)
+
+;; This is the file you would like to use for your main todo list.
+;; Customize to taste. (This is not a special Org variable, but rather
+;; just a variable we are defining for future use.)
+
+(setq my-todo-file "~/Desktop/todo.org")
+
+;; We make a convenient function for accessing your todo list.
+
+(defun my-goto-todo-file ()
+  "Go to `my-todo-file'."
+  (interactive)
+  (find-file my-todo-file))
+
+;; We will also want to set up some keybindings. While it is not hard
+;; to do this right out of the box, there is a nice library called
+;; `bind-key' which makes it easier.
+
+(straight-use-package 'bind-key)
+
+;; You can call the `my-goto-todo-file' function with M-x
+;; my-goto-todo-file. But it is faster to use C-c t, so let's do that
+;; instead.
+
+(bind-key "C-c t" 'my-goto-todo-file)
+
+;; You can get to the agenda view in Org by typing M-x org-agenda.
+;; However, since we want to view the agenda more easily, we also bind
+;; that command to the sequence C-c a.
+
+(bind-key "C-c a" 'org-agenda)
+
+;; Similarly, we'd like to capture a TODO item by pressing just C-c c
+;; rather than M-x org-capture.
+
+(bind-key "C-c c" 'org-capture)
+
+;; We want Org files to be shown as an indented tree, rather than just
+;; as a flat file. This enables that functionality by default; you can
+;; toggle it temporarily by running M-x org-indent-mode.
+;;
+;; To explain the following code, Emacs allows for editing different
+;; kinds of files by using a different "major mode" for each. Org
+;; files are edited using `org-mode', for example. When you edit an
+;; Org file, that causes `org-mode-hook' to be run. Thus, by adding
+;; functions to this hook, you can customize what happens when editing
+;; Org files. In this case, we tell Emacs to run the function
+;; `org-indent-mode', which activates the "minor mode" by that name.
+;; Minor modes are like additional filters that can be enabled within
+;; a particular major mode, which customize various behaviors. By
+;; activating `org-indent-mode', we tell Org to show us an indented
+;; view rather than a flat one.
+
+(add-hook 'org-mode-hook 'org-indent-mode)
+
+;; Tell the agenda view to display your todo list.
+
+(setq org-agenda-files (list my-todo-file))
+
+;; Show the week starting at the current day, not starting at Monday.
+
+(setq org-agenda-start-on-weekday nil)
+
+;; Hide scheduled items on the agenda view until the day they are
+;; scheduled.
+
+(setq org-agenda-skip-deadline-prewarning-if-scheduled t)
+
+;; A convenience function for making Org capture templates.
+
+(defun my-org-capture (keys name heading &rest subheadings)
+  "Make an Org capture template.
+KEYS is the key sequence to trigger the template, a
+string. (Remember that if you want to use a multi-key sequence,
+you also have to add another entry to the template list defining
+the sub-menu triggered by pressing the first key.)
+
+NAME is the text that will be displayed in the capture popup.
+
+HEADING is the top-level heading that the capture template will
+insert into.
+
+SUBHEADINGS, which are optional, are subheadings under the
+top-level heading."
+  `(,keys ,name entry (file+olp ,my-todo-file ,heading ,@subheadings) "* TODO %?"))
+
+;; These are shortcuts for using Org capture.
+
+(setq org-capture-templates
+      (list
+       (my-org-capture "e" "An example template" "Heading" "Subheading")
+       (list "p" "A prefix key")
+       (my-org-capture "pe" "An example with a prefix key" "Just a heading")))
+
+;; If you close Emacs while a clock is running, then the next time you
+;; open an Org file, ask if the clock should be resumed (instead of
+;; forgetting about it).
+
+(setq org-clock-persist t)
+(org-clock-persistence-insinuate)
